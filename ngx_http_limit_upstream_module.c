e2e0be3535d591ef3d986ec390a3ec1089239a25
Ricardo Acosta <ricardo.acosta@zankyou.com>
2020-02-01 10:33:03 +0100

    fix ngx_del_timer and update patch for ngixn 1.17.8

--- ngx_http_limit_upstream_module.c
+++ ngx_http_limit_upstream_module.c
@@ -327,8 +327,6 @@ ngx_http_limit_upstream_cleanup(void *data)
         ctx->r->read_event_handler = wnode->read_event_handler;
         ctx->r->write_event_handler = wnode->write_event_handler;
 
-        ngx_del_timer(ctx->r->connection->read);
-
         if (wnode->r_timer_set) {
             if (ctx->r->connection->read->timedout) {
                 ctx->r->read_event_handler(ctx->r);
